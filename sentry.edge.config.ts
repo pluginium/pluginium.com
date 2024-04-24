c754a7c5c8bbe5bc806fea27278dfdf4f2b885ab
Morgan Spencer <morgan@spencercreative.co>
2024-04-24 11:35:27 -0400

    Add Sentry

---
+++ sentry.edge.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://300043804dd9129fb70ef981b5baed97@o4507142208946176.ingest.us.sentry.io/4507142209077248',
+
+  enabled: process.env.NODE_ENV === 'production' ? true : false,
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
---
+++ sentry.server.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://300043804dd9129fb70ef981b5baed97@o4507142208946176.ingest.us.sentry.io/4507142209077248',
+
+  enabled: process.env.NODE_ENV === 'production' ? true : false,
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+})
